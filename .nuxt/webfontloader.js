f2b56e526259dfd00ee3a863e75333105dadb557
vitko1984 <knnaksay@gmail.com>
2020-12-03 15:04:04 +0300

    init nick_nuxt

---
+++ .nuxt/webfontloader.js
@@ -0,0 +1,6 @@
+
+import WebFont from 'webfontloader'
+
+const options = {"events":false,"google":{"families":["Montserrat:400,500,600:cyrillic&display=swap"]},"timeout":5000}
+
+WebFont.load(options)
